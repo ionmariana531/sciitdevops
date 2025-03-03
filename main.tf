a2d036fef658ca850be2f66093c4c050b29524a3
ionmariana531 <ionmariana531@yahoo.com>
2025-03-03 21:03:01 +0000

    new files to be added

--- main.tf
+++ main.tf
@@ -1,4 +1,77 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
-  acl    = var.acl
+provider "aws" {
+  region = var.aws_region
 }
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  name            = "eks-cluster-${basename(path.cwd)}"
+  cluster_version = "1.31"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  tags = {
+    Environment = "Dev"
+    Project     = "AWS-EKS"
+  }
+}
+
+################################################################################
+# VPC Module
+################################################################################
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+
+  tags = local.tags
+}
+
+################################################################################
+# EKS Module
+################################################################################
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = local.name
+  cluster_version = local.cluster_version
+
+  cluster_endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    general-purpose = {
+      instance_types = ["t3.medium"]
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 1
+    }
+  }
+
+  tags = local.tags
+}
+
