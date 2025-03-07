064ca65fafd2bf13c65a84e735bb3bc5f54cecb4
ionmariana531 <ionmariana531@yahoo.com>
2025-03-07 18:37:14 +0000

    Files configuration changed

--- main.tf
+++ main.tf
@@ -1,136 +1,87 @@
-###############################################################################
-# Required version was added
-###############################################################################
-#terraform {
-#  required_providers {
-#    aws = {
-#      source  = "hashicorp/aws"
-#      version = "3.74.0"
-#    }
-#  }
-#}
-
 provider "aws" {
-  region = var.aws_region
+  region = "eu-west-1"
 }
 
-resource "aws_iam_role" "devops_role" {
-  name               = "DevOps-Role"
+# IAM Role pentru GitHub Actions OIDC
+resource "aws_iam_role" "github_oidc_role" {
+  name = "GitHub-OIDC-Role"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = {
-          Federated = "arn:aws:iam::329599661643:oidc-provider/token.actions.githubusercontent.com"
-        }
-        Action    = "sts:AssumeRoleWithWebIdentity"
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-          }
-          StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:ionmariana531/sciitdevops:ref:refs/heads/main"
-          }
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = "arn:aws:iam::329599661643:oidc-provider/token.actions.githubusercontent.com"
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:ionmariana531/sciitdevops:ref:refs/heads/add-terraform-github-actions-Mariana-fe"
         }
       }
-    ]
+    }]
   })
 }
-# Permisiuni pentru rol
-resource "aws_iam_role_policy" "devops_role_policy" {
-  name   = "DevOpsRolePolicy"
-  role   = aws_iam_role.devops_role.name
+
+# Politici pentru acces la AWS
+resource "aws_iam_role_policy" "github_oidc_policy" {
+  name = "GitHubOIDCPolicy"
+  role = aws_iam_role.github_oidc_role.id
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action   = "s3:*"
+        Action   = ["sts:AssumeRole"]
         Effect   = "Allow"
         Resource = "*"
       },
       {
-        Action   = "ec2:*"
+        Action   = ["s3:*", "ec2:*", "eks:*"]
         Effect   = "Allow"
         Resource = "*"
-      },
+      }
     ]
   })
 }
 
-data "aws_availability_zones" "available" {}
-
-locals {
-  name            = "eks-cluster-${basename(path.cwd)}"
-  cluster_version = "1.31"
-
-  vpc_cidr = "10.0.0.0/16"
-  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
-
-  tags = {
-    Environment = "Dev"
-    Project     = "AWS-EKS"
-  }
-}
-
-################################################################################
-# VPC Module
-################################################################################
-
+# Creare VPC pentru EKS
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
 
-  name = local.name
-  cidr = local.vpc_cidr
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
 
-  azs             = local.azs
-  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
-  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
+  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
-
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
-  }
-
-  tags = local.tags
 }
 
-################################################################################
-# EKS Module
-################################################################################
-
+# Creare cluster EKS
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = local.name
-  cluster_version = local.cluster_version
-
-  cluster_endpoint_public_access           = true
-  enable_cluster_creator_admin_permissions = true
+  cluster_name    = "eks-cluster"
+  cluster_version = "1.31"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = {
-    general-purpose = {
+    app-nodes = {
       instance_types = ["t3.medium"]
-      min_size       = 1
-      max_size       = 3
-      desired_size   = 1
+      min_size       = 2
+      max_size       = 5
+      desired_size   = 2
     }
   }
-
-  tags = local.tags
 }
 
-# Trigger GitHub Actions
-# Trigger GitHub Actions
-# Trigger GitHub Actions
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
+}
+
