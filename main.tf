625e52715ca5f485060fb8105ff6bcbd46463435
ionmariana531 <ionmariana531@yahoo.com>
2025-03-14 13:13:23 +0000

    Check initialization of EC2

--- main.tf
+++ main.tf
@@ -12,12 +12,19 @@ data "aws_subnet" "Subnet-VPC" {
   id = "subnet-0592d5af417aa893f"
 }
 
-# Select the SG
-data "aws_security_group" "VPC-SG" {
-  filter {
-    name = "group-name"
-    values = ["VPC needed for final exam"]
-  }  
+# Use Existing Security Group
+data "aws_security_group" "existing_sg" {
+  id = "sg-008e2201608e83f75"
+}
+
+# Add SSH rule to the existing Security Group (DANGEROUS: Open to the internet!)
+resource "aws_security_group_rule" "open_ssh" {
+  security_group_id = data.aws_security_group.existing_sg.id
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"] # ⚠️ DANGEROUS: SSH allowed from anywhere
 }
 
 # Select the key
@@ -25,40 +32,37 @@ data "aws_key_pair" "existing_key" {
   key_name = "app-ssh-key"
 }
 
+# Create EC2 Instances
 resource "aws_instance" "Instance1" {
-  ami = "ami-03fd334507439f4d1"
-  instance_type = "t2.micro"
-  subnet_id = data.aws_subnet.Subnet-VPC.id
-  vpc_security_group_ids = [data.aws_security_group.VPC-SG.id]
-  key_name = data.aws_key_pair.existing_key.key_name
-  
+  ami                    = "ami-03fd334507439f4d1"
+  instance_type          = "t2.micro"
+  subnet_id              = data.aws_subnet.Subnet-VPC.id
+  vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
+  key_name               = data.aws_key_pair.existing_key.key_name
+
   tags = {
     Name = "Instance-1"
   }
 }
 
-
 #resource "aws_instance" "Instance2" {
-#  ami = "ami-03fd334507439f4d1"
-#  instance_type = "t2.micro"
-#  subnet_id = data.aws_subnet.Subnet-VPC.id
-#  vpc_security_group_ids = [data.aws_security_group.VPC-SG.id]
-#  key_name = data.aws_key_pair.existing_key.key_name
-  
+#  ami                    = "ami-03fd334507439f4d1"
+#  instance_type          = "t2.micro"
+#  subnet_id              = data.aws_subnet.Subnet-VPC.id
+#  vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
+#  key_name               = data.aws_key_pair.existing_key.key_name
+
 #  tags = {
 #    Name = "Instance-2"
-
 #  }
 #}
 
-
 #resource "aws_instance" "Instance3" {
-#  ami = "ami-03fd334507439f4d1"
-#  instance_type = "t2.micro"
-#  subnet_id = data.aws_subnet.Subnet-VPC.id
-#  vpc_security_group_ids = [data.aws_security_group.VPC-SG.id]
-#   key_name = data.aws_key_pair.existing_key.key_name
-
+#  ami                    = "ami-03fd334507439f4d1"
+#  instance_type          = "t2.micro"
+#  subnet_id              = data.aws_subnet.Subnet-VPC.id
+#  vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
+#  key_name               = data.aws_key_pair.existing_key.key_name
 
 #  tags = {
 #    Name = "Instance-3"
