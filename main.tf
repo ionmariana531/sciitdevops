72fd2a07278c3e8f3b3844850523bb0cecc96d7c
ionmariana531 <ionmariana531@yahoo.com>
2025-03-17 13:58:34 +0000

    Instance2 added

--- main.tf
+++ main.tf
@@ -45,17 +45,17 @@ resource "aws_instance" "Instance1" {
   }
 }
 
-#resource "aws_instance" "Instance2" {
-#  ami                    = "ami-03fd334507439f4d1"
-#  instance_type          = "t2.micro"
-#  subnet_id              = data.aws_subnet.Subnet-VPC.id
-#  vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
-#  key_name               = data.aws_key_pair.existing_key.key_name
+resource "aws_instance" "Instance2" {
+  ami                    = "ami-03fd334507439f4d1"
+  instance_type          = "t2.medium"
+  subnet_id              = data.aws_subnet.Subnet-VPC.id
+  vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
+  key_name               = data.aws_key_pair.existing_key.key_name
 
-#  tags = {
-#    Name = "Instance-2"
-#  }
-#}
+  tags = {
+    Name = "Instance-2"
+  }
+}
 
 #resource "aws_instance" "Instance3" {
 #  ami                    = "ami-03fd334507439f4d1"
